671e0a94c93f58826b0888971073c2eb46546496
shubhangamjha <shubhangamjha25@gmail.com>
2025-03-30 12:48:50 +0530

    Added memory.py to backend

---
+++ src/backend/memory.py
@@ -0,0 +1,17 @@
+rom
+collections
+import deque
+
+
+class ChatMemory:
+    def __init__(self, max_size=10):
+        self.history = deque(maxlen=max_size)
+
+    def add_message(self, message):
+        self.history.append(message)
+
+    def get_history(self):
+        return list(self.history)
+
+
+chat_memory = ChatMemory()
